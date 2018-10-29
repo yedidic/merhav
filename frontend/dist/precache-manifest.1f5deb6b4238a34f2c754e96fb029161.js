0a1daccb2f35593240c28f98b61ccae79a1f0b57
yedidic <yedidic@gmail.com>
2018-10-29 12:29:29 +0200

    as b4

---
+++ frontend/dist/precache-manifest.1f5deb6b4238a34f2c754e96fb029161.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4ea022d80a94ba98bd2c",
+    "url": "/js/chunk-vendors.409df069.js"
+  },
+  {
+    "revision": "d58e47b5aae45915dde2",
+    "url": "/js/app.a0f818ec.js"
+  },
+  {
+    "revision": "bdcb05c9644be3ea524a",
+    "url": "/js/about.16f12a91.js"
+  },
+  {
+    "revision": "3ac637f892c7d5d7c0004293a2a0bd34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c980940392587c8d7d325c07965ebda",
+    "url": "/fonts/Rubik-Regular.6c980940.ttf"
+  },
+  {
+    "revision": "87b4e0d5acc5093dfca4c4355b809749",
+    "url": "/fonts/Rubik-Bold.87b4e0d5.ttf"
+  },
+  {
+    "revision": "d58e47b5aae45915dde2",
+    "url": "/css/app.4b6e0ca5.css"
+  }
+];
